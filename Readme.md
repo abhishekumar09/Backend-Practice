3496d0b7544e9de80b500d68f43225f5e5c10c88
abhishekumar09 <2003abhishek.09@gmail.com>
2024-08-22 18:11:56 +0530

    pass the data to browser and verify the data

--- Readme.md
+++ Readme.md
@@ -2,10 +2,14 @@ we learn and practice about -:
 Q1) How to set and read the cookie 
 Q2) How to use bcrypt for password encryption and decryption
 solution-: 
+// we encrypt password by help of bcyrpt , salt and hash
 // salt ek random string hoti hei 
 // sbse phle ek salt bnte ho lmbi string hoti hei aur phie ek hash bnte ho jo ki password ko change krke kuch bhi lik deta hei and then we mix the salt with hash phir ek random string bnti jo decrypt nhi  hoti hei 
 
 Q3) What is JWT and how to import and export data in it. 
+solution -:
+ What is JWT?
+Ans -: JWT stands for JSON Web Token, and it is a commonly used stateless user authentication standard used to securely transmit information between client and server in a JSON format. A JWT is encoded and not encrypted by default. It is digitally signed using a secret key known only to the server.
 
 
 
@@ -15,3 +19,17 @@ Cookie- from server to save a data in browser
 
 // Advantage of cookie
 when we go thorugh a browser to any other routes the cookie also go there by default
+
+
+
+
+password = passuguhword
+
+encrypted password =    $2b$10$KDfaCJg8tx./I520m359TOgYxtVSSXsqZsaOMYu9T.Cz3oLis0K/e    
+
+
+
+
+
+// What is cookieparser ?
+Ans-: Cookie-parser is a middleware for handling HTTP cookies in Node. It can be used to extract cookie data from HTTP requests and convert it into a usable format for server-side code. The data can then be parsed to extract individual values like the cookie name, value, and expiration date. The data can be stored in an object or other data structure that can be easily accessed and manipulated by the server-side code.
--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
@@ -9,14 +11,26 @@ app.use(cookieParser());
 // To hash a password:
 // Technique 1 (generate a salt and hash on separate function calls):
 
-app.get('/', function(req,res){
+// app.get('/', function(req,res){
 
-bcrypt.genSalt(10, function(err, salt) {                                  // 10 means random number are total 10
-    bcrypt.hash("password", salt, function(err, hash) {
-        // Store hash in your password DB.
-    });
-});
-})
+// bcrypt.genSalt(10, function(err, salt) {                                  // 10 means random number are total 10
+//     bcrypt.hash("passuguhword", salt, function(err, hash) {
+//        console.log(hash); // Store hash in your password DB.
+//     });
+// });
+// })
+
+
+//    // To check a password:
+// app.get('/', function(req,res){
+//     // Load hash from your password DB.
+//     bcrypt.compare("passuguhword",   "$2b$10$KDfaCJg8tx./I520m359TOgYxtVSSXsqZsaOMYu9T.Cz3oLis0K/e"  , function(err, result) {
+//         console.log(result);// result == true
+//     });
+    
+   
+//     })
+
 
 // app.get('/', function(req,res){
 //    res.cookie("name", "Abhi");                               // for the set of cookie    // kisi bhi route se cookie kaise set krte hein aur read bhi kaise krte ho
@@ -27,5 +41,23 @@ bcrypt.genSalt(10, function(err, salt) {                                  // 10 
 //    console.log(req.cookies);            // use for read the cookie in console
 //     res.send("read page");
 //  })
+
+
+
+
+// jwt
+app.use(cookieParser());
+app.get("/", function(req,res){
+ let token = jwt.sign({ email: 'abhi@gmail.com' }, 'secret');
+res.cookie("token",token);
+ //console.log(token);                                             // result ek lmbi si string(token) aaygi jise hm broser ko share krenge 
+res.send("done");              // broswer pr share krdia string
+})
+
+app.get("/read", function(req,res){
+   // console.log(req.cookies.token);
+let data= jwt.verify(req.cookies.token, "secret");            // for verify the data same or not
+console.log(data);
+})
 
 app.listen(3000);
