1fd35b94737c3513d3142e2a3c93f53cf1cca299
abhishekumar09 <2003abhishek.09@gmail.com>
2024-08-22 16:42:35 +0530

    salt and hash update

--- Readme.md
+++ Readme.md
@@ -1,4 +1,17 @@
 we learn and practice about -:
-1) How to set Cookie
-2) How to use bcrypt for password encryption and decryption
-3) What is JWT and how to import and export data in it. 
+Q1) How to set and read the cookie 
+Q2) How to use bcrypt for password encryption and decryption
+solution-: 
+// salt ek random string hoti hei 
+// sbse phle ek salt bnte ho lmbi string hoti hei aur phie ek hash bnte ho jo ki password ko change krke kuch bhi lik deta hei and then we mix the salt with hash phir ek random string bnti jo decrypt nhi  hoti hei 
+
+Q3) What is JWT and how to import and export data in it. 
+
+
+
+
+Cookie- from server to save a data in browser
+
+
+// Advantage of cookie
+when we go thorugh a browser to any other routes the cookie also go there by default
--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 
 const app = express();
 
+app.use(cookieParser());
+
+
+// To hash a password:
+// Technique 1 (generate a salt and hash on separate function calls):
+
 app.get('/', function(req,res){
-   res.cookie("name", "Abhi");                                   // kisi bhi route se cookie kaise set krte hein aur read bhi kaise krte ho
-   res.send("done");
+
+bcrypt.genSalt(10, function(err, salt) {                                  // 10 means random number are total 10
+    bcrypt.hash("password", salt, function(err, hash) {
+        // Store hash in your password DB.
+    });
+});
 })
+
+// app.get('/', function(req,res){
+//    res.cookie("name", "Abhi");                               // for the set of cookie    // kisi bhi route se cookie kaise set krte hein aur read bhi kaise krte ho
+//    res.send("done");
+// })
+
+// app.get('/read', function(req,res){
+//    console.log(req.cookies);            // use for read the cookie in console
+//     res.send("read page");
+//  })
 
 app.listen(3000);
